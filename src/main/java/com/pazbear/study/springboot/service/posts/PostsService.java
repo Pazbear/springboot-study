1804f1f4a54e993e4c8b50c43afe2549d3e08629
Pazbear <mkcho1997@daum.net>
2022-04-07 19:12:04 +0900

    게시판 화면 만들기

--- src/main/java/com/pazbear/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/pazbear/study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.pazbear.study.springboot.service.posts;
 
 import com.pazbear.study.springboot.domain.posts.Posts;
 import com.pazbear.study.springboot.domain.posts.PostsRepository;
+import com.pazbear.study.springboot.web.dto.PostsListResponseDto;
 import com.pazbear.study.springboot.web.dto.PostsResponseDto;
 import com.pazbear.study.springboot.web.dto.PostsSaveRequestDto;
 import com.pazbear.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/test/java/com/pazbear/study/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.pazbear.study.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void 메인페이지_로딩(){
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트 공부");
+    }
+}
