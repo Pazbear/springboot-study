c4e030334ca9f2e46a8d2685d1433e3f86048700
Pazbear <mkcho1997@daum.net>
2022-04-07 14:53:00 +0900

    JPA Auditing - BaseTimeEntity 추가

--- src/test/java/com/pazbear/study/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/pazbear/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +20,27 @@ public class PostsRepositoryTest {
     @AfterEach
     public void cleanup(){
         postsRepository.deleteAll();
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0,0,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
     @Test
